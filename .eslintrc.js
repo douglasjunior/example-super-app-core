a8364f94f6ff8fd8c33fbb73db79e4264ffebc97
Douglas Nassif Roma Junior <douglas.junior@db1.com.br>
2022-06-28 16:12:40 -0300

    Adiciona husky para impedir commits de códigos que não passam nos testes

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,4 +13,7 @@ module.exports = {
       },
     },
   ],
+  global: {
+    jest: false,
+  },
 };
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  testEnvironment: 'jsdom',
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!example-super-app-.*|@react-navigation|@react-native|react-native)',
+  ],
+  setupFiles: ['<rootDir>/jest.setup.js'],
 };
