259c8e190be4cf8975e9b4e8103c65efe5854c68
Douglas Nassif Roma Junior <douglas.junior@db1.com.br>
2022-07-01 17:13:16 -0300

    Atualiza context e configura setup do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {AppProvider} from 'example-super-app-context';
-import React from 'react';
-import {SafeAreaView} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { AppProvider, setup } from 'example-super-app-context';
+import React, { useEffect } from 'react';
+import { SafeAreaView } from 'react-native';
 
 import RootStackNavigator from './navigation/RootStackNavigator';
 
 const App: React.FC = () => {
+
+  useEffect(() => {
+    setup({
+      appName: 'Super App',
+      reactotron: true,
+    });
+  }, []);
+  
   return (
     <AppProvider>
       <NavigationContainer>
-        <SafeAreaView style={{flex: 1}}>
+        <SafeAreaView style={{ flex: 1 }}>
           <RootStackNavigator />
         </SafeAreaView>
       </NavigationContainer>
